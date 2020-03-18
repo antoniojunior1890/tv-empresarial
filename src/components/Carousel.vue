90da635115b49c3a27baa4cb124fa47aad91e77a
antonio.aroucha <antonio.aroucha@armateus.com.br>
2020-03-18 09:03:39 -0300

    D - funçao random para usar no 'interval'

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
+    {{ slide }}
     <b-carousel
-      id="carousel-1"
-      :interval="4000"
+      id="carousel"
+      v-model="slide"
+      :interval="interval"
       controls
       indicators
       background="#ababab"
@@ -10,19 +12,13 @@
       img-height="480"
       style="text-shadow: 1px 1px 2px #333;"
     >
-      <!-- Slides with image only -->
-      <!-- <b-carousel-slide
-        v-for="item in images"
-        :key="item.id"
-        :img-src="item.image"
-        :style="{ maxHeight: maxHeight }"
-      ></b-carousel-slide>-->
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
       <b-carousel-slide v-for="item in images" :key="item.id">
         <template v-slot:img>
-          <img :style="{ maxHeight: maxHeight , minHeight : maxHeight }" :src="item.image" alt="image slot" />
+          <img
+            :style="{ maxHeight: maxHeight , minHeight : maxHeight }"
+            :src="item.image"
+            :alt="item.name"
+          />
         </template>
       </b-carousel-slide>
     </b-carousel>
@@ -34,22 +30,32 @@ export default {
   data() {
     return {
       maxHeight: screen.height + "px",
-      images: []
+      images: [],
+      interval: 2000,
+      slide: 0,
     };
   },
   created() {
     this.$http
       .get("/all.json")
       .then(res => {
-        this.images = res.data;
-
-        // eslint-disable-next-line
-        // console.log(this.images);
+        this.images = { ...res.data, born: "123" };
       })
       .catch(function(error) {
         /* eslint-disable */
         console.log(error);
       });
+  },
+  methods: {
+    getRndInteger(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+  },
+  watch: {
+    slide : function() {
+      this.interval = this.getRndInteger(1, 5) * 1000
+      console.log(this.interval)
+    }
   }
 };
 </script>
